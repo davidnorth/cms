3e98ee347dd2a30dfa2360896bf3b55704027d1d
David North <davidnorth@gmail.com>
2009-05-30 16:32:31 +0100

    fix jquery sortable, try using data attribute to store ajax url

--- public/javascripts/admin.js
+++ public/javascripts/admin.js
@@ -1,6 +1,13 @@
 $(document).ready(function(){
   
-  $(".sortable").sortable();
+  $(".sortable").sortable({
+    axis: 'y',
+    stop: function(s){
+      url = $(this).attr('data-remote') + '?' + $(this).sortable('serialize');
+      data = {authenticity_token: authenticity_token}
+      $.post(url, data );
+      }
+  });
 
   $(".tree_table").treeTable();
 
