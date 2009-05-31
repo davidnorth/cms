e417051c16b7da450e91e6da56c0e06f22f254dd
David North <davidnorth@gmail.com>
2009-05-31 08:03:29 +0100

    added jquery ui files. use dialog widget for new page dialog

--- public/javascripts/admin.js
+++ public/javascripts/admin.js
@@ -11,6 +11,37 @@ $(document).ready(function(){
 
   $(".tree_table").treeTable();
 
+  $.ui.dialog.defaults.bgiframe = true;
+  $(".dialog").each(function(dialog){
+    form = $(this).find('form').get(0);
+    buttons = {};
+    $(this).find('button').each(function(){
+      buttons[this.innerHTML] = function(){ form.submit() };
+      $(this).remove();
+    })
+    $(this).dialog({ autoOpen: false, buttons: buttons })
+  });
+
 })
 
 
+
+
+function showNewPageDialog()
+{
+  args = $.makeArray(arguments);
+  parent_id = args.shift();
+
+  $('#page_title').get(0).value = '';
+  $('#new_page_dialog').dialog('open');
+
+  ts = $('#type_select').get(0);
+  ts.innerHTML = '';
+
+  $.each(args, function(){
+    option = new Option();
+    option.value = this;
+    option.text = this.replace('_',' ');
+    ts.options[ts.options.length] = option;    
+  });
+}
